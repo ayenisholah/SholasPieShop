23ce32e92fc49891106667f6184a13595c325d08
Shola Ayeni <ayenisholah@yahoo.com>
2020-04-18 00:00:52 +0100

    created mock data for pie categories

---
+++ Models/MockCategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace SholasPie.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category
+                {
+                    CategoryId=1, CategoryName="Fruit Pie", Description="All fruits pie"
+                },
+                new Category
+                {
+                    CategoryId=2, CategoryName="Cheese Cakes", Description="Cheesy all the way"
+                },
+                new Category
+                {
+                    CategoryId=3, CategoryName="Seasonal Pies", Description="Get in the mood for a seasonal pie"
+                }
+            };
+    }
+}
