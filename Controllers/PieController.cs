117fde3c555c20d5690b2455afd07d089fde3ac8
Shola Ayeni <ayenisholah@yahoo.com>
2020-04-18 00:27:26 +0100

    created pie controller with view action

---
+++ Controllers/PieController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SholasPie.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SholasPie.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _pieRepository = pieRepository;
+        }
+        public ViewResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
