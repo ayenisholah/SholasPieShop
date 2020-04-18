4e63d523eaf8ce3d5bf17c288dff9ace955c2de1
Shola Ayeni <ayenisholah@yahoo.com>
2020-04-18 17:47:26 +0100

    created app db context

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SholasPie.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Pie> Pies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
